4181b0d1482fb7903420085634ad701f44a747e1
Yoshihiro Ueda <yoshihiroueda@gmail.com>
2015-08-23 13:08:33 +0900

    Updated with cross validation sample

--- trials.R
+++ trials.R
@@ -1,1 +1,7 @@
+pml.training <- read.csv("./data/pml-training.csv")
+
 pml.testing <- read.csv("~/OneDrive/Projects/courses/08_PracticalMachineLearning/assignment/pml-testing.csv")
+library("caret")
+
+model0 <- train(classe ~ . , data=pml.training)
+
